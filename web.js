44c83d6967b751ce8d72518b59dc765025109d90
Mike Nobil <mikenobil@gmail.com>
2013-07-16 18:04:22 +0000

    web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send( 
-      fs.readFileSync('index.html', buf.toString(); 
+      buf.toString( fs.readFileSync('index.html'); 
   );
 });
 
