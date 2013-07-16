2c828eb4e1d7ab785f72f44dc16bb545c2ce6b93
Mike Nobil <mikenobil@gmail.com>
2013-07-16 18:22:57 +0000

    web.js

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require( 'fs' );     
-    var content = fs.readFileSync('index.html', 'utf8');
+    response.send(
+	var fs = require( 'fs' );     
+	var content = fs.readFileSync('index.html', 'utf8');
+    );
 });
 
 var port = process.env.PORT || 5000;
