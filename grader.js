c4ae96ab909a24e74950240586df49fc3aa4c3e8
Mike Nobil <mikenobil@gmail.com>
2013-07-16 20:25:29 +0000

    .

--- grader.js
+++ grader.js
@@ -61,10 +61,14 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var dl = function(curl){
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL to index.html', dl(clone(assertFileExists)), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
