86311a7585d5864fd93e4e48e9ca4f3d5986710e
Mike Nobil <mikenobil@gmail.com>
2013-07-16 18:31:52 +0000

    web.js

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(
+    response.send() {
 	var fs = require( 'fs' );     
 	var content = fs.readFileSync('index.html', 'utf8');
-    );
+    });
 });
 
 var port = process.env.PORT || 5000;
